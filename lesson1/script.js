c4e56f7b133e3be33e9b3bf7cc93426602406d02
cold-snowflake <cold.snowflake01@gmail.com>
2023-08-29 00:59:14 +0200

    Hello JavaScript

---
+++ lesson1/script.js
@@ -0,0 +1,5 @@
+let name = prompt('What is your name?')
+let hello = "Hello, "
+let greeting = "! How are you? This is JavaScript world!"
+alert(hello + name.charAt(0).toUpperCase()+name.slice(1) + greeting )
+ 
