8689a53e04684779224b7ea0309b8caa8c34fc61
cold-snowflake <cold.snowflake01@gmail.com>
2023-10-16 10:51:00 +0200

    lesson7

---
+++ lesson7/script.js
@@ -0,0 +1,73 @@
+// function getSum(num){
+//     return num < 2 ? num: getSum(num - 1) + num
+// }
+
+// const summ = getSum(5)
+// console.log(summ)
+
+// function badSum(num){
+//     function lessTwo(num) {
+//     return num - 2
+//     }
+//     function lessOne(num){
+//         return num -1 + lessTwo(num)
+//     }
+//     return num + lessOne(num)
+// }
+
+// console.log(badSum(3))
+
+// function fibbanacci(num){
+//     return num <= 2 ? 1 : fibbanacci(num - 1) + fibbanacci(num - 2)
+// }
+// console.log(fibbanacci(7))
+
+// const ArrowSum = num => num * 41
+
+// console.log(ArrowSum(2))
+
+// const setDocumentTitel = (newTitel='') => {
+//     if(newTitel.length > 3){
+//     order.title = newTitel
+//     return true
+//     }
+//     console.log('Too short length')
+//     return false
+// }
+// const setOwner = (newOwner='') => {
+//     if(
+//     newOwner.toLocaleLowerCase().includes('bastard') ||
+//     newOwner.length < 4 ||
+//     newOwner.split() !== 2
+//     ){
+//         console.log('PLS enter correct name')
+//         return false
+//     }
+//     order.owner = newOwner
+//     return true
+// }
+// const title = 'My order'
+
+// const order = {
+//     title,
+//     id: "0358746",
+//     created: new Date('10/11/2019'),
+//     owner: 'Tetiana',
+//     setDocumentTitel,
+//     setOwner
+// }
+
+// const productFive = (num, callback) => {
+//     return callback(num * 5)
+// }
+
+// myCallBack = num => console.log(num)
+
+// productFive(4, myCallBack)
+
+//IIFE
+
+(function(args){
+    console.log('args', args)
+    console.log('HELLE I`M IIFE')
+})('Hello I`m args')
